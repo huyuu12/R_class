d0d2c87d19712c29b93d0a542e505b69e15f966b
huyuu12 <135413462+huyuu12@users.noreply.github.com>
2023-09-11 16:33:44 +0900

    1강

---
+++ R_1/Class_1.R
@@ -0,0 +1,104 @@
+####1강####
+
+library(ggplot2)
+library(abc)
+a = 2
+b = 3
+x = 5
+a
+print(a)
+print(x)
+print("Hi")
+print(3)
+print("3")
+print("ㅁㅁㄴㄴㄴㅇㅇㅇㅇㄹㄹㄹ")
+print(1000000000)
+print(0.0000000001)
+
+
+print(3+1)
+print(4/2)
+print(5-7)
+print(100000*123414)
+print(1000^100)
+print(5>3)
+print(1==2)
+print(6!=3)
+print((1==3)|(3>6))
+print((1==3)&(3>6))
+print(isTRUE(1))
+print(!2)
+T
+
+integer_var = 5
+numeric_var = 3.141592
+complex_var = 2 + 3i
+character_var = "Hello, world!"
+logical_var = T
+another_logical_var = F
+NULL_var = NULL
+NA_var = NA
+Negate_inf = -Inf
+positive_inf = Inf
+nan = 0/0
+
+typeof(integer_var)
+typeof(numeric_var)
+typeof(complex_var)
+typeof(character_var)
+typeof(logical_var)
+typeof(another_logical_var)
+typeof(NULL_var)
+typeof(NA_var)
+typeof(Negate_inf)
+typeof(positive_inf)
+typeof(nan)
+
+
+mode(integer_var)
+mode(numeric_var)
+mode(complex_var)
+mode(character_var)
+mode(logical_var)
+mode(another_logical_var)
+mode(NULL_var)
+mode(NA_var)
+mode(Negate_inf)
+mode(positive_inf)
+mode(nan)
+
+
+sin(1)
+cos(1)
+tan(1)
+sinh(1)
+exp(1)
+log(1)
+log(1, 10)
+
+
+a = c(1, 2, 3, 4, 5)
+sum(c(1,2,3,4,5,6,7,8,9,10))
+seq(from=1, to=5, by=2)     #1부터 5까지 2씩 증가
+seq(1,5, 2)
+seq(0,1,length.out=1000)
+
+rep(c(1,2,3), times=2)
+rep(c(1,2,3), each = 2)
+
+
+
+
+a = 3
+print(a)
+print(3)
+print("Hello")
+
+paste0(3+4, 4, 5, 6, 7)
+print(paste0(3+4, 4, 5, 6, 7))
+
+paste0(3, 4, 5, 6, 7, sep = "-")
+paste0(3+4, 4, 5, 6, 7, sep="-")
+
+####2강####
+??ggplt2
