bc57787ff4c0fa827a32464da93ff417d7a7d726
huyuu12 <135413462+huyuu12@users.noreply.github.com>
2023-09-24 16:42:01 +0900

    Add files via upload

---
+++ 2주차/R_class 2주차.R
@@ -0,0 +1,95 @@
+x1 <- 2
+x2 <<- c(1,2,3)
+x3 = c("a","b","c")
+x1
+x2
+x3
+
+a<-3
+b=4
+c=7
+d=a+b
+
+print(a)
+print(d)
+print("a")
+
+3+2
+3-2
+3*2
+3/2
+3^2
+3%%2
+3 %/% 2
+
+5<5
+5<=5
+5 == 5
+5!= 5
+!TRUE
+TRUE | FALSE
+TRUE & FALSE
+isTRUE(TRUE)
+
+integer_var= 5
+numeric_var = 3.14
+complex_var = 2+3i
+character_var = "Hello, world!"
+logical_var = TRUE
+another_logical_var = FALSE
+NULL_var = NULL
+NA_var = NA
+negative_inf = Inf
+positive_Inf = Inf
+nan = 0/0
+
+typeof(integer_var)
+typeof(numeric_var)
+typeof(complex_var)
+typeof(character_var)
+typeof(logical_var)
+typeof(another_logical_var)
+typeof(NULL_var)
+typeof(negative_inf)
+typeof(positive_Inf)
+typeof(nan)
+
+mode(integer_var)
+mode(numeric_var)
+mode(complex_var)
+mode(character_var)
+mode(logical_var)
+mode(another_logical_var)
+mode(NULL_var)
+mode(negative_inf)
+mode(positive_Inf)
+mode(nan)
+
+sin(1)
+cos(1)
+tan(1)
+sinh(1)
+log(1)
+log(100,base=10)
+
+a=c(1,2,3,4,5,6)
+sum(c(1,2,3,4,5,6,7,8,9,10))
+seq(from = 1, to = 5, by =2)
+seq(1,5,by=2)
+seq(0,1,length.out=1000)
+rep(c(1,2,3),times=2)
+rep(c(1,2,3), each=2)
+
+a=3
+print(a)
+print(3)
+print("Helllo")
+
+paste(3,4,5,6,7)
+print(paste(3,4,5,6,7))
+
+paste0(3+4,4,5,6,7)
+print(paste0(3+4,4,5,6,7))
+
+paste(3,4,5,6,7,sep="-")
+paste0(3+4,4,5,6,7,sep="-")
